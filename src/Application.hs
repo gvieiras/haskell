7aa807518fe04c47d8f63c48ee44ef4820b3b87a
gvieiras <ogabrielvieira96@gmail.com>
2018-06-14 15:20:39 -0300

    importando os handlers usuario e login

--- src/Application.hs
+++ src/Application.hs
@@ -42,6 +42,8 @@ import Handler.Artista
 import Handler.Album
 import Handler.Evento
 import Handler.Selo
+import Handler.Login
+import Handler.Usuario
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
