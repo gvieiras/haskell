b116e61988de27c2b99a68e7eb0700bcefa82b18
gvieiras <ogabrielvieira96@gmail.com>
2018-06-14 15:07:53 -0300

    gerando divs do form de logar

---
+++ src/Handler/Login.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Login where
+
+import Import
+import Text.Julius 
+import Text.Lucius
+import           Yesod.Auth
+import Database.Persist.Postgresql
+
+formLogin :: Form (Text, Text)
+formLogin = renderDivs $ (,) 
+        <$> areq emailField "Email: " Nothing
+        <*> areq passwordField "Senha: " Nothing
+        
